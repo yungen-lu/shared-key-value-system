69ac8b45672e32c0cca2d3103bef882893e8aace
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2023-03-02 11:04:35 +0800

    feat: add swagger docs

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -4,8 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "github.com/yungen-lu/shared-key-value-list-system/docs"
 )
 
+// @title		Shared Key Value List System API
+// @version	1.0
+// @basePath	/v1
 func NewRouter(handler *gin.Engine) {
 	handler.Use(gin.Recovery())
 	handler.GET("/healthz", func(ctx *gin.Context) {
@@ -15,4 +21,5 @@ func NewRouter(handler *gin.Engine) {
 	{
 		v1.GET("/page", PageHandler)
 	}
+	handler.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
