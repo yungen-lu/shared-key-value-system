2e6f0d2bb8037c9bd0da3b3a271a55f5bd1abbc0
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2023-03-12 21:38:47 +0800

    feat: update list usecase

--- internal/usecase/list.go
+++ internal/usecase/list.go
@@ -2,33 +2,46 @@ package usecase
 
 import (
 	"context"
+	"time"
 
 	"github.com/yungen-lu/shared-key-value-list-system/internal/domain"
 )
 
-type listUseCase struct {
-	l domain.ListRepo
-	p domain.PageRepo
+type ListUseCase struct {
+	l              domain.ListRepo
+	p              domain.PageRepo
+	contextTimeout time.Duration
 }
 
-var _ List = (*listUseCase)(nil)
+var _ List = (*ListUseCase)(nil)
 
-func NewListUseCase() *listUseCase {
-	return &listUseCase{}
+func NewListUseCase(listRepo domain.ListRepo, pageRepo domain.PageRepo) *ListUseCase {
+	return &ListUseCase{
+		l: listRepo,
+		p: pageRepo,
+	}
 }
 
-func (uc *listUseCase) GetHeads(ctx context.Context) ([]domain.List, error) {
-	return nil, nil
+func (uc *ListUseCase) GetHeads(ctx context.Context) ([]domain.List, error) {
+	c, cancel := context.WithTimeout(ctx, uc.contextTimeout)
+	defer cancel()
+	return uc.l.GetAll(c)
 }
 
-func (uc *listUseCase) GetHeadByID(ctx context.Context, id int32) (domain.List, error) {
-	return domain.List{}, nil
+func (uc *ListUseCase) GetHeadByID(ctx context.Context, id int32) (domain.List, error) {
+	c, cancel := context.WithTimeout(ctx, uc.contextTimeout)
+	defer cancel()
+	return uc.l.GetByID(c, id)
 }
 
-func (uc *listUseCase) GetPages(ctx context.Context) ([]domain.Page, error) {
-	return nil, nil
+func (uc *ListUseCase) GetPages(ctx context.Context) ([]domain.Page, error) {
+	c, cancel := context.WithTimeout(ctx, uc.contextTimeout)
+	defer cancel()
+	return uc.p.GetAll(c)
 }
 
-func (uc *listUseCase) GetPageByID(ctx context.Context, id int32) (domain.Page, error) {
-	return domain.Page{}, nil
+func (uc *ListUseCase) GetPageByID(ctx context.Context, id int32) (domain.Page, error) {
+	c, cancel := context.WithTimeout(ctx, uc.contextTimeout)
+	defer cancel()
+	return uc.p.GetByID(c, id)
 }
