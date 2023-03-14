537e9ca1934d269e9725cddb9475252850f3a3a0
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2023-03-14 20:28:10 +0800

    feat: add routes

--- internal/controller/http/v1/head.go
+++ internal/controller/http/v1/head.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/charmbracelet/log"
 	"github.com/gin-gonic/gin"
+	"github.com/yungen-lu/shared-key-value-list-system/internal/domain"
 	"github.com/yungen-lu/shared-key-value-list-system/internal/usecase"
 )
 
@@ -12,39 +14,67 @@ type headRoutes struct {
 	list usecase.List
 }
 
-func newHeadRoutes(handler *gin.RouterGroup) {
-	r := &headRoutes{}
+func NewHeadRoutes(handler *gin.RouterGroup, listUseCase usecase.List) {
+	r := &headRoutes{
+		list: listUseCase,
+	}
 	h := handler.Group("/head")
 	{
-		h.GET("/", r.getAll)
-		h.GET("/:id", r.getByID)
+		h.GET("", r.getAll)
+		h.GET("/:key", r.getByKey)
+		h.POST("", r.create)
+		h.PUT("/:key", r.update)
 	}
 }
 
-//	@Summary		Get all heads
-//	@Description	test
-//	@Accept			json
-//	@Produce		json
-//	@Success		200	{array}	domain.List
-//	@Falure			500 {object}
-//	@Router			/head [get]
+// @Summary		Get all heads
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Success		200	{array}	domain.List
+// @Falure			500 {object}
+// @Router			/head [get]
 func (r *headRoutes) getAll(c *gin.Context) {
 	lists, err := r.list.GetHeads(c)
 	if err != nil {
+		log.Error("http - v1 - head - getAll - GetHeads", err)
 		c.JSON(http.StatusInternalServerError, gin.H{})
 		return
 	}
 	c.JSON(http.StatusOK, lists)
 }
 
-//	@Summary		Get a head by id
-//	@Description	test
-//	@Accept			json
-//	@Produce		json
-//	@Param			id	path		int	true	"Head ID"
-//	@Success		200	{object}	domain.List
-//	@Falure			500 {object}
-//	@Router			/head/{id} [get]
+// @Summary		Get a head by key
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Param			key	path		int	true	"Head Key"
+// @Success		200	{object}	domain.List
+// @Falure			500 {object}
+// @Router			/head/{key} [get]
+func (r *headRoutes) getByKey(c *gin.Context) {
+	key, ok := c.Params.Get("key")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	list, err := r.list.GetHeadByKey(c, key)
+	if err != nil {
+		log.Error("http - v1 - head - getByKey - GetHeadByKey", "err", err, "key", key)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, list)
+}
+
+// @Summary		Get a head by id
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Param			id	path		int	true	"Head ID"
+// @Success		200	{object}	domain.List
+// @Falure			500 {object}
+// @Router			/head/{id} [get]
 func (r *headRoutes) getByID(c *gin.Context) {
 	idStr, ok := c.Params.Get("id")
 	if !ok {
@@ -58,8 +88,73 @@ func (r *headRoutes) getByID(c *gin.Context) {
 	}
 	list, err := r.list.GetHeadByID(c, int32(id))
 	if err != nil {
+		log.Error("http - v1 - head - getByID - GetHeadByID", "err", err)
 		c.JSON(http.StatusInternalServerError, gin.H{})
 		return
 	}
 	c.JSON(http.StatusOK, list)
+}
+
+type createHeadRequest struct {
+	// ID         int32 `json:"id" binding:"required"`
+	Key        string `json:"key" binding:"required"`
+	NextPageID *int32 `json:"next_page_id"`
+}
+
+// @Summary		Create a head
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Param
+// @Success		200	{object}	domain.List
+// @Falure			500 {object}
+// @Router			/head [post]
+func (r *headRoutes) create(c *gin.Context) {
+	var req createHeadRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	err := r.list.CreateHead(c, domain.List{Key: req.Key, NextPageID: req.NextPageID})
+	if err != nil {
+		log.Error("http - v1 - head - create - CreateHead", "err", err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{})
+}
+
+type updateHeadRequest struct {
+	Key        *string `json:"key"`
+	NextPageID *int32  `json:"next_page_id"`
+}
+
+// @Summary Update a head
+// @Description test
+// @Accept json
+// @Produce json
+// @Param head body updateHeadRequest true "Head"
+// @Success 200 {object} model.Head
+// @Failure 500 {object}
+// @Router /head [put]
+
+func (r *headRoutes) update(c *gin.Context) {
+	key, ok := c.Params.Get("key")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	var req updateHeadRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	// err := r.list.UpdateHeadByKey(c, key, domain.ListUpdate{Key: req.Key, NextPageID: req.NextPageID})
+	err := r.list.UpdateHeadByKey(c, key, domain.List{Key: *req.Key, NextPageID: req.NextPageID})
+	if err != nil {
+		log.Error("http - v1 - head - update - UpdateHeadByKey", "err", err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{})
 }
--- internal/controller/http/v1/page.go
+++ internal/controller/http/v1/page.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/charmbracelet/log"
 	"github.com/gin-gonic/gin"
+	"github.com/yungen-lu/shared-key-value-list-system/internal/domain"
 	"github.com/yungen-lu/shared-key-value-list-system/internal/usecase"
 )
 
@@ -12,39 +14,67 @@ type pageRoutes struct {
 	list usecase.List
 }
 
-func newPageRoutes(handler *gin.RouterGroup) {
-	r := &pageRoutes{}
+func NewPageRoutes(handler *gin.RouterGroup, listUseCase usecase.List) {
+	r := &pageRoutes{
+		list: listUseCase,
+	}
 	h := handler.Group("/page")
 	{
-		h.GET("/", r.getAll)
-		h.GET("/:id", r.getByID)
+		h.GET("", r.getAll)
+		h.GET("/:key", r.getByKey)
+		h.POST("", r.create)
+		h.PUT("/:key", r.update)
 	}
 }
 
-//	@Summary		Get all pages
-//	@Description	test
-//	@Accept			json
-//	@Produce		json
-//	@Success		200	{array}	domain.Page
-//	@Falure			500 {object}
-//	@Router			/page [get]
+// @Summary		Get all pages
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Success		200	{array}	domain.Page
+// @Falure			500 {object}
+// @Router			/page [get]
 func (r *pageRoutes) getAll(c *gin.Context) {
 	pages, err := r.list.GetPages(c)
 	if err != nil {
+		log.Error("http - v1 - page - getAll - GetPages", "err", err)
 		c.JSON(http.StatusInternalServerError, gin.H{})
 		return
 	}
 	c.JSON(http.StatusOK, pages)
 }
 
-//	@Summary		Get a page by id
-//	@Description	test
-//	@Accept			json
-//	@Produce		json
-//	@Param			id	path		int	true	"page ID"
-//	@Success		200	{object}	domain.Page
-//	@Falure			500 {object}
-//	@Router			/page/{id} [get]
+// @Summary		Get a page by key
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Param			key	path		string	true	"page Key"
+// @Success		200	{object}	domain.Page
+// @Falure			500 {object}
+// @Router			/page/{key} [get]
+func (r *pageRoutes) getByKey(c *gin.Context) {
+	key, ok := c.Params.Get("key")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	page, err := r.list.GetPageByKey(c, key)
+	if err != nil {
+		log.Error("http - v1 - page - getByKey - GetPageByKey", "err", err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, page)
+}
+
+// @Summary		Get a page by id
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Param			id	path		int	true	"page ID"
+// @Success		200	{object}	domain.Page
+// @Falure			500 {object}
+// @Router			/page/{id} [get]
 func (r *pageRoutes) getByID(c *gin.Context) {
 	idStr, ok := c.Params.Get("id")
 	if !ok {
@@ -58,8 +88,72 @@ func (r *pageRoutes) getByID(c *gin.Context) {
 	}
 	page, err := r.list.GetPageByID(c, int32(id))
 	if err != nil {
+		log.Error("http - v1 - page - getByID - GetPageByID", "err", err)
 		c.JSON(http.StatusInternalServerError, gin.H{})
 		return
 	}
 	c.JSON(http.StatusOK, page)
+}
+
+type createPageRequest struct {
+	Key        string `json:"key" binding:"required"`
+	NextPageID *int32 `json:"next_page_id"`
+}
+
+// @Summary		Create a page
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Param
+// @Success		200	{object}	domain.Page
+// @Falure			500 {object}
+// @Router			/page [post]
+
+func (r *pageRoutes) create(c *gin.Context) {
+	var createPageRequest createPageRequest
+	if err := c.ShouldBindJSON(&createPageRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	err := r.list.CreatePage(c, domain.Page{Key: createPageRequest.Key, NextPageID: createPageRequest.NextPageID})
+	if err != nil {
+		log.Error("http - v1 - page - create - CreatePage", "err", err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{})
+}
+
+type updatePageRequest struct {
+	Key        string `json:"key"`
+	NextPageID *int32 `json:"next_page_id"`
+}
+
+// @Summary		Update a page
+// @Description	test
+// @Accept			json
+// @Produce		json
+// @Param page body updatePageRequest true "Page"
+// @Success		200	{object}	domain.Page
+// @Falure			500 {object}
+// @Router			/page{key} [put]
+
+func (r *pageRoutes) update(c *gin.Context) {
+	key, ok := c.Params.Get("key")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	var updatePageRequest updatePageRequest
+	if err := c.ShouldBindJSON(&updatePageRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	err := r.list.UpdatePageByKey(c, key, domain.Page{Key: updatePageRequest.Key, NextPageID: updatePageRequest.NextPageID})
+	if err != nil {
+		log.Error("http - v1 - page - update - UpdatePageByKey", "err", err)
+		c.JSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{})
 }
